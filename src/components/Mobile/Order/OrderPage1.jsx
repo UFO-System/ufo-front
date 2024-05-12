9024d2f99a425c4a203c93670c716643e41eada7
MoJeongHwan <mjhmo4062@gmail.com>
2024-05-12 17:10:34 +0900

    경고창, 테이블 넘버, 화면전환 업데이트

--- src/components/Mobile/Order/OrderPage1.jsx
+++ src/components/Mobile/Order/OrderPage1.jsx
@@ -8,56 +8,58 @@ import { Input as BaseInput } from '@mui/base/Input';
 import { styled } from '@mui/system';
 import Input from '@mui/material/Input';
 import { useHistory } from 'react-router-dom';
-import "./css/OrderPage.css";
+import "./App.css"
 
 const OrderPage1 = () => {
+  
+  
   // 매장 이름
-  const name = "소웨";
-  // 테스트
+  const name = 'UFO'
+
   // 메뉴 이름
-  const menu1 = "사과";
-  const menu2 = "포도";
-  const menu3 = "망고";
+  const menu1 = '사과'
+  const menu2 = '포도'
+  const menu3 = '망고'
 
   // 메뉴 가격
-  const price1 = 1000;
-  const price2 = 2000;
-  const price3 = 3000;
+  const price1 = 1000
+  const price2 = 2000
+  const price3 = 3000
 
   // 메뉴 수량 더하기
-  const [count1, setCount1] = useState(0);
-  function plus1() {
-    setCount1(count1 + 1);
+  const [count1, setCount1] = useState(0)
+  function plus1(){
+    setCount1(count1 + 1)
   }
-  const [count2, setCount2] = useState(0);
-  function plus2() {
-    setCount2(count2 + 1);
+  const [count2, setCount2] = useState(0)
+  function plus2(){
+    setCount2(count2 + 1)
   }
-  const [count3, setCount3] = useState(0);
-  function plus3() {
-    setCount3(count3 + 1);
+  const [count3, setCount3] = useState(0)
+  function plus3(){
+    setCount3(count3 + 1)
   }
 
   // 메뉴 수량 빼기
-  function minus1() {
-    if (count1 == 0) {
-      count1 == 0;
-    } else {
-      setCount1(count1 - 1);
+  function minus1(){
+    if(count1 == 0){
+      count1 == 0
+    }else{
+      setCount1(count1 - 1)
     }
   }
-  function minus2() {
-    if (count2 == 0) {
-      count2 == 0;
-    } else {
-      setCount2(count2 - 1);
+  function minus2(){
+    if(count2 == 0){
+      count2 == 0
+    }else{
+      setCount2(count2 - 1)
     }
   }
-  function minus3() {
-    if (count3 == 0) {
-      count3 == 0;
-    } else {
-      setCount3(count3 - 1);
+  function minus3(){
+    if(count3 == 0){
+      count3 == 0
+    }else{
+      setCount3(count3 - 1)
     }
   }
   // 페이지 전환
@@ -66,24 +68,25 @@ const OrderPage1 = () => {
   // 경고창
   const [value, setValue] = useState('');
   const [showWarning, setShowWarning] = useState(false);
-    
+  
   const handleChange = (e) => {
-    const inputValue = e.target.value;
+  const inputValue = e.target.value;
     setValue(inputValue);
     // 입력값이 없는 경우에는 경고창을 표시합니다.
     setShowWarning(inputValue === '');
   };
 
+
+
+
   return (
-      <div className="app">
-
-        {/* 로고 */}
-        <AppBar position="static">
-          <h4 className="name">{name}</h4>
-        </AppBar>
-
-        {/* 테이블 넘버 입력과 경고창 */}
-        <Input 
+    // 로고 앱바
+    <div className='app'>
+    <AppBar position="static">
+      <h4 className='name'>{name}&nbsp;&nbsp;&nbsp;</h4>
+    </AppBar>
+      {/* 테이블 넘버 텍스틑 상자 */}
+      <Input 
         className='input'
         placeholder="테이블 넘버를 입력하세요."
         type="text"
@@ -92,109 +95,48 @@ const OrderPage1 = () => {
         />
       {showWarning && <p style={{ color: 'red' }}>테이블 넘버를 입력해주세요.</p>}
       <hr></hr>
-
-      {/* 메뉴 */}
-        <div className="menu">
-          <h5>
-            {menu1} {count1} 개
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={minus1}
-              style={{ background: "red" }}
-            >
-              {" "}
-              -{" "}
-            </Button>
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={plus1}
-            >
-              {" "}
-              +{" "}
-            </Button>
-          </h5>
-          <p>{price1} 원</p>
-        </div>
-        <div className="menu">
-          <h5>
-            {menu2} {count2} 개
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={minus2}
-              style={{ background: "red" }}
-            >
-              {" "}
-              -{" "}
-            </Button>
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={plus2}
-            >
-              {" "}
-              +{" "}
-            </Button>
-          </h5>
-          <p>{price2} 원</p>
-        </div>
-        <div className="menu">
-          <h5>
-            {menu3} {count3} 개
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={minus3}
-              style={{ background: "red" }}
-            >
-              {" "}
-              -{" "}
-            </Button>
-            <Button
-              className="OrderPageBtn"
-              variant="contained"
-              onClick={plus3}
-            >
-              {" "}
-              +{" "}
-            </Button>
-          </h5>
-          <p>{price3} 원</p>
-        </div>
-        {/* 음식 수량, 가격 계산 */}
-        <div style={{position: "fixed", bottom: 0 ,width: "100%"}}>
-        <div className="bottom">
-          <h4>
-            총 수량: {count1 + count2 + count3}개 | 총 금액:{" "}
-            {price1 * count1 + price2 * count2 + price3 * count3}원
-          </h4>
-        </div>
-        <AppBar
-          position="static"
-          style={{ height: "80px", paddingTop: "15px", display: "flex" }}
-        >
-        {/* 화면 전환 */}
-          <center>
-            <Button
-              className="orderBt"
-              onClick={() => {
-                navigate("/OrderPage2", {
-                  state: {
-                    total: price1 * count1 + price2 * count2 + price3 * count3,
-                    name: name,
-                  },
-                });
-              }}
-              style={{ color: "white", boxShadow: "1px 1px 5px black" }}
-            >
-              주문하기
-            </Button>
-          </center>
-        </AppBar>
-        </div>
+      {/* 음식 메뉴 */}
+      <div className='menu'>
+        <h5>{menu1} {count1} 개 
+        <Button variant="contained" onClick={minus1} style={{background:'red'}}> - </Button>
+        <Button variant="contained" onClick={plus1}> + </Button></h5>
+        <p>{price1} 원</p>
       </div>
-  );
-};
+      <div className='menu'>
+        <h5>{menu2} {count2} 개 
+        <Button variant="contained" onClick={minus2} style={{background:'red'}}> - </Button>
+        <Button variant="contained" onClick={plus2}> + </Button></h5>
+        <p>{price2} 원</p>
+      </div>
+      <div className='menu'>
+        <h5>{menu3} {count3} 개 
+        <Button variant="contained" onClick={minus3} style={{background:'red'}}> - </Button>
+        <Button variant="contained" onClick={plus3}> + </Button></h5>
+        <p>{price3} 원</p>
+      </div>
+      {/* 음식 수량, 금액 계산 */}
+      <div style={{position: "fixed", bottom: 0 ,width: "100%"}}>
+      <div className='bottom'>
+        <h4>총 수량: {count1 + count2 + count3}개 | 총 금액: {price1 * count1 + price2 * count2 + price3 * count3}원</h4>
+      </div>
+      {/* 버튼 이벤트, 화면 전환 */}
+      <AppBar position="static" 
+      style={{height:'80px', paddingTop:'15px', display:'flex'}}>
+      <center>
+      <Button className='orderBt' onClick={()=>{
+      if (value === '') {
+        alert('테이블 넘버를 입력해주세요');  
+      } else {
+        console.log('입력값:', value);{navigate('/OrderPage2', 
+        {state:{total : price1 * count1 + price2 * count2 + price3 * count3, name : name,
+         value:value
+        }})}}}}
+      style={{color: 'white', boxShadow:'1px 1px 5px black'}}> 
+      주문하기</Button>
+      </center>
+      </AppBar>
+      </div>
+      </div> 
+  )
+}
 export default OrderPage1;
--- src/components/Mobile/Order/OrderPage2.jsx
+++ src/components/Mobile/Order/OrderPage2.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import { useLocation } from "react-router-dom";
-import "./css/OrderPage.css";
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import OrderPage1 from './OrderPage1';
+import Container from '@mui/material/Container';
+import {useLocation, useNavigate} from 'react-router-dom'
+import Button from '@mui/material/Button';
+import "./App.css"
 
 const OrderPage2 = () => {
+  
   const location = useLocation();
   console.log(location);
-  const total = location.state.total;
-  const name = location.state.name;
-  return (
-    <div className="app">
-      <AppBar position="static">
-        <h4 className="name">{name}</h4>
-        {total}
-      </AppBar>
-    </div>
-  );
-};
+  const total= location.state.total;
+  const name = location.state.name
+  const value = location.state.value
 
-export default OrderPage2;
+  const number = ' 은행 1234567890'
+  const navigate =useNavigate();
+  return (
+    
+    <div className='app'>
+      <AppBar position="static">
+      <h4 className='name'>{name}</h4>
+      </AppBar>
+      <div className='menu'>
+        <p> 총 가격: {total} 원</p>
+        <p> 계좌번호: {number}</p>
+        <p> 테이블 넘버: {value}</p>
+      </div>
+      <div style={{position: "fixed", bottom: 0 ,width: "100%"}}>
+      <AppBar position="static" 
+      style={{height:'80px', paddingTop:'15px', display:'flex'}}>
+      <center>
+      <Button className='orderBt' 
+      style={{color: 'white', boxShadow:'1px 1px 5px black'}}
+      onClick={()=>{navigate(-1)}}>
+      뒤로</Button>
+      </center>
+      </AppBar>
+      </div>
+    </div>
+    
+  )
+}
+
+export default OrderPage2
+
