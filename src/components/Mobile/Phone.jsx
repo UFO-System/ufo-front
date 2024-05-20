79e88a585bab02f866d8551693ad52aa428d3012
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-05-20 13:15:29 +0900

    ✨ Feat: 컴퓨터화면에서 핸드폰화면으로 전환 기능
    ✨컴퓨터화면에서 url을 수정하여 이동하는 것보다 버튼을 만드는게 나을것 같아서 버튼 생성

--- src/components/Mobile/Phone.jsx
+++ src/components/Mobile/Phone.jsx
@@ -1,37 +1,60 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./css/Phone.css";
+import LogoutTopBar from "../Desktop/commons/TopBar/LogoutTopBar";
 
 const Phone = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const navigate = useNavigate();
+
+  const handleOrderPageClick = () => {
+    navigate("/OrderPage1");
+  };
+
+  const handleKitchenPageClick = () => {
+    navigate("/KitchenPage");
+  };
+
   return (
     <div className="button-container">
+      <LogoutTopBar />
       <Box
         sx={{
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: isMobile ? "column" : "row",
           gap: "2vh",
+          padding: "1vw",
           alignItems: "center",
           justifyContent: "center",
-          width: "100%",
-          minHeight: "98vh",
+          width: "98vw",
+          maxHeight: "50vh",
         }}
       >
         <Button
           variant="contained"
-          onClick={() => navigate("/OrderPage1")}
+          onClick={handleOrderPageClick}
           sx={{
-            height: "48vh",
-            width: "95%",
+            width: "100%",
+            height: isMobile ? "48vh" : "48vh",
           }}
         >
           <Typography variant="h3">주문화면</Typography>
         </Button>
         <Button
           variant="contained"
-          onClick={() => navigate("/KitchenPage")}
-          sx={{ height: "48vh", width: "95%" }}
+          onClick={handleKitchenPageClick}
+          sx={{
+            width: "100%",
+            height: isMobile ? "48vh" : "48vh",
+          }}
         >
           <Typography variant="h3">부엌화면</Typography>
         </Button>
