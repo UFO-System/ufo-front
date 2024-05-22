f308448b4de916c360f6789e09fc68397071c1c9
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-05-22 13:56:10 +0900

    Feat: 주문관리 정렬 기능

--- src/components/Desktop/OrderManagePage/OrderManage.jsx
+++ src/components/Desktop/OrderManagePage/OrderManage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   TableContainer,
   Table,
@@ -10,53 +10,90 @@ import {
   TextField,
   Button,
   Box,
-} from '@mui/material';
+  TableSortLabel,
+} from "@mui/material";
 
 const OrderManage = () => {
-  const [orders, setOrders] = useState([
-    { id: 1, food: '어묵탕 외 3건', depositor: '배준재', amount: 20000 },
-    { id: 2, food: '닭발 외 2건', depositor: '김호진', amount: 15000 },
-    { id: 3, food: '어묵탕 외 1건', depositor: '옥준서', amount: 10000 },
-    { id: 4, food: '닭발 외 2건', depositor: '모정환', amount: 20000 },
-    { id: 5, food: '파전 외 3건', depositor: '김건우', amount: 30000 },
-    { id: 6, food: '닭발 외 4건', depositor: '최일한', amount: 50000 },
-    { id: 7, food: '김치전 외 1건', depositor: '류태웅', amount: 17000 },
-    { id: 8, food: '닭발 외 2건', depositor: '김재영', amount: 18000 },
-    { id: 9, food: '어묵탕', depositor: '손원일', amount: 8000 },
-    { id: 10, food: '김치찜', depositor: '배준재', amount: 15000 },
-    { id: 11, food: '만두 외 2건', depositor: '김호진', amount: 15000 },
-    { id: 12, food: '음료', depositor: '옥준서', amount: 2000 },
-  ]); // 주문 목록 상태
+  const [data, setData] = useState([
+    { id: 1, food: "어묵탕 외 3건", depositor: "배준재", amount: 20000 },
+    { id: 2, food: "닭발 외 2건", depositor: "김호진", amount: 15000 },
+    { id: 3, food: "어묵탕 외 1건", depositor: "옥준서", amount: 10000 },
+    { id: 4, food: "닭발 외 2건", depositor: "모정환", amount: 20000 },
+    { id: 5, food: "파전 외 3건", depositor: "김건우", amount: 30000 },
+    { id: 6, food: "닭발 외 4건", depositor: "최일한", amount: 50000 },
+    { id: 7, food: "김치전 외 1건", depositor: "류태웅", amount: 17000 },
+    { id: 8, food: "닭발 외 2건", depositor: "김재영", amount: 18000 },
+    { id: 9, food: "어묵탕", depositor: "손원일", amount: 8000 },
+    { id: 10, food: "김치찜", depositor: "배준재", amount: 15000 },
+    { id: 11, food: "만두 외 2건", depositor: "김호진", amount: 15000 },
+    { id: 12, food: "음료", depositor: "옥준서", amount: 2000 },
+  ]);
 
-  const [depositor, setDepositor] = useState(''); // 입금자명
-  const [amount, setAmount] = useState(''); // 입금 금액
+  const [depositor, setDepositor] = useState(""); // 입금자명
+  const [amount, setAmount] = useState(""); // 입금 금액
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("");
+
+  const handleRequestSort = (property) => {
+    let newOrderBy = property;
+    let newOrder = "desc";
+    if (orderBy === property && order === "desc") {
+      newOrder = "asc";
+    }
+
+    setOrder(newOrder);
+    setOrderBy(newOrderBy);
+  };
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
+  const getComparator = (order, orderBy) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const sortedSales = stableSort(data, getComparator(order, orderBy));
 
   const handleReject = (id) => {
-    // 거절 버튼 클릭 시 반응
     const confirmDelete = window.confirm(`주문번호 ${id} 를 거절하시겠습니까?`);
 
     if (confirmDelete) {
-      // 주문 거절 후 지워지게 함
-      const updatedOrders = orders.filter((order) => order.id !== id);
-      setOrders(updatedOrders);
+      const updatedOrders = data.filter((order) => order.id !== id);
+      setData(updatedOrders);
     }
   };
 
   const handleAccept = (id) => {
-    // 수락 버튼 클릭 시 반응
     const confirmAccept = window.confirm(`주문번호 ${id} 를 수락하시겠습니까?`);
 
     if (confirmAccept) {
-      // 주문 수락 후 지워지게 함
-      const updatedOrders = orders.filter((order) => order.id !== id);
-      setOrders(updatedOrders);
+      const updatedOrders = data.filter((order) => order.id !== id);
+      setData(updatedOrders);
     }
   };
 
   return (
-    // 주문 내역 테이블 구역
     <Box
-      sx={{ display: 'flex', justifyContent: 'space-between', height: '100vh' }}
+      sx={{ display: "flex", justifyContent: "space-between", height: "100vh" }}
     >
       <TableContainer component={Paper}>
         <Table>
@@ -65,46 +102,70 @@ const OrderManage = () => {
               <TableCell
                 colSpan={5}
                 align="center"
-                sx={{ backgroundColor: '#002884', color: 'white' }}
+                sx={{ backgroundColor: "#002884", color: "white" }}
               >
                 주문내역
               </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                주문번호
+              <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
+                <TableSortLabel
+                  active={orderBy === "id"}
+                  direction={orderBy === "id" ? order : "asc"}
+                  onClick={() => handleRequestSort("id")}
+                >
+                  주문번호
+                </TableSortLabel>
               </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                주문음식
+              <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
+                <TableSortLabel
+                  active={orderBy === "food"}
+                  direction={orderBy === "food" ? order : "asc"}
+                  onClick={() => handleRequestSort("food")}
+                >
+                  주문음식
+                </TableSortLabel>
               </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                입금자명
+              <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
+                <TableSortLabel
+                  active={orderBy === "depositor"}
+                  direction={orderBy === "depositor" ? order : "asc"}
+                  onClick={() => handleRequestSort("depositor")}
+                >
+                  입금자명
+                </TableSortLabel>
               </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                입금 금액
+              <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
+                <TableSortLabel
+                  active={orderBy === "amount"}
+                  direction={orderBy === "amount" ? order : "asc"}
+                  onClick={() => handleRequestSort("amount")}
+                >
+                  입금 금액
+                </TableSortLabel>
               </TableCell>
-              <TableCell sx={{ fontWeight: 'bold', textAlign: 'center' }}>
+              <TableCell sx={{ fontWeight: "bold", textAlign: "center" }}>
                 비고
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {orders.map((order) => (
+            {sortedSales.map((order) => (
               <TableRow key={order.id}>
-                <TableCell sx={{ textAlign: 'center' }}>{order.id}</TableCell>
-                <TableCell sx={{ textAlign: 'center' }}>{order.food}</TableCell>
-                <TableCell sx={{ textAlign: 'center' }}>
+                <TableCell sx={{ textAlign: "center" }}>{order.id}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>{order.food}</TableCell>
+                <TableCell sx={{ textAlign: "center" }}>
                   {order.depositor}
                 </TableCell>
-                <TableCell sx={{ textAlign: 'center' }}>
+                <TableCell sx={{ textAlign: "center" }}>
                   {order.amount}
                 </TableCell>
-                <TableCell sx={{ textAlign: 'center' }}>
+                <TableCell sx={{ textAlign: "center" }}>
                   <Button
                     variant="contained"
                     color="error"
                     onClick={() => handleReject(order.id)}
-                    sx={{ marginRight: '5px' }}
+                    sx={{ marginRight: "5px" }}
                   >
                     거절
                   </Button>
@@ -122,14 +183,14 @@ const OrderManage = () => {
         </Table>
       </TableContainer>
 
-      <Box // 입금 내역 테이블 구역 (로그 창 구현은 아직 x)
+      <Box
         sx={{
-          display: 'inline-block',
-          width: '40%',
-          backgroundColor: 'lightgray',
-          padding: '5px',
-          marginLeft: '10px',
-          marginRight: '10px',
+          display: "inline-block",
+          width: "40%",
+          backgroundColor: "lightgray",
+          padding: "5px",
+          marginLeft: "10px",
+          marginRight: "10px",
         }}
       >
         <TableContainer component={Paper}>
@@ -139,7 +200,7 @@ const OrderManage = () => {
                 <TableCell
                   colSpan={2}
                   align="center"
-                  sx={{ backgroundColor: '#002884', color: 'white' }}
+                  sx={{ backgroundColor: "#002884", color: "white" }}
                 >
                   입금내역
                 </TableCell>
