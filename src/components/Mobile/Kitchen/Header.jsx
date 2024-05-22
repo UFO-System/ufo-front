20cfcf69090d9602bc001e0374b31fe8383403fc
Ok Junseo <dhrwnstj@naver.com>
2024-05-22 21:55:14 +0900

    Modify: 반응형으로 변경 Add: 페이지네이션 추가

--- src/components/Mobile/Kitchen/Header.jsx
+++ src/components/Mobile/Kitchen/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import { Button, Pagination, Stack } from "@mui/material";
 import "./css/KitchenPage.css";
-import { Box } from "@mui/material";
-import { Button } from "@mui/material";
 
 const Header = () => {
   return (
     <div>
       <Button
+        className="headerButton"
         variant="contained"
         size="large"
         sx={{
@@ -14,21 +14,31 @@ const Header = () => {
           top: "0.7vh",
           left: "5vw",
           width: "15vw",
+          fontSize: "1vw",
           height: "50px",
           borderRadius: 5,
+          backgroundColor: "#7AA2E3",
           "&:hover": {
             backgroundColor: "#4285f4",
           },
-          "&:not(:hover)": {
-            backgroundColor: "#7AA2E3",
-            transition: "background-color 0.5s",
-          },
+          transition: "background-color 0.5s",
         }}
       >
-        {" "}
         주문 되돌리기
       </Button>
+      <Pagination
+      showFirstButton 
+      showLastButton
+      count={10} 
+      size="large"
+      sx={{
+        position: "absolute", 
+        top: "1.5vh",
+        right: "5vw",
+        color: "white"
+        }}/>
     </div>
+    
   );
 };
 
--- src/components/Mobile/Kitchen/OrderBoard.jsx
+++ src/components/Mobile/Kitchen/OrderBoard.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import "./css/KitchenPage.css";
 import { Box } from "@mui/material";
+import "./css/KitchenPage.css";
 
 const OrderBoard = ({ tableNo, values }) => {
-  const menuNameLength = (menuName) => {
-    if (menuName.length >= 6) {
-      return <>{menuName.substring(0, 6)}</>;
-    } else {
-      return menuName;
-    }
-  };
+  const menuNameLength = (menuName) => (menuName.length > 6 ? menuName.substring(0, 6) : menuName);
 
   return (
     <Box
+      className="orderBoardBox"
       sx={{
+        top: "-1vh",
         color: "#3c2a2c",
         position: "relative",
         border: "3px solid #4D83BD",
@@ -21,18 +17,13 @@ const OrderBoard = ({ tableNo, values }) => {
         height: "88vh",
         borderRadius: 5,
         display: "inline-block",
-        top: "-1vh",
-        left: `1.5vw`,
         margin: "0.4%",
         boxSizing: "border-box",
+        backgroundColor: "#E7E7E7",
+        transition: "background-color 0.5s",
         "&:hover": {
           backgroundColor: "#8A7E72",
-          transition: "0.5s",
           cursor: "pointer",
-        },
-        "&:not(:hover)": {
-          backgroundColor: "#E7E7E7",
-          transition: "background-color 0.5s",
         },
       }}
     >
@@ -41,11 +32,11 @@ const OrderBoard = ({ tableNo, values }) => {
           style={{
             display: "flex",
             justifyContent: "space-between",
-            fontSize: "25px",
+            fontSize: "1.8vw",
             width: "100%",
-            padding: "5px",
+            padding: "0.5vw",
             fontWeight: "bolder",
-            marginBottom: "10px",
+            marginBottom: "1vh",
             position: "relative",
           }}
         >
@@ -54,14 +45,14 @@ const OrderBoard = ({ tableNo, values }) => {
             <br />
             <span>Table No. {tableNo}</span>
           </div>
-          <span style={{ position: "absolute", top: "25px", right: "35px", fontSize: "30px"}}>
+          <span style={{ position: "absolute", top: "3vh", right: "2vw", fontSize: "1.8vw" }}>
             16:00
           </span>
         </div>
-        <div style={{ padding: "5px" }}>
+        <div>
           {values.map((order, index) => (
-            <div className="tableDiv" key={index} style={{ margin: "30px auto" }}>
-              {menuNameLength(order.menu_name)} {order.order_cnt === 1 ? "" : order.order_cnt}
+            <div className="tableDiv" key={index} style={{ marginTop: "3vh" }}>
+              {menuNameLength(order.menu_name)} {order.order_cnt > 1 ? order.order_cnt : ""}
             </div>
           ))}
         </div>
