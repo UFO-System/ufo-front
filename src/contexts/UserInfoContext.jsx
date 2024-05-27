be08d20796a63107a1c9b4c42d8ca33424b03005
hojin535 <123153695+hojin535@users.noreply.github.com>
2024-05-28 01:00:15 +0900

    Feat: QR생성기 및 다운로드 페이지 구현

--- src/contexts/UserInfoContext.jsx
+++ src/contexts/UserInfoContext.jsx
@@ -4,6 +4,7 @@ export const UserInfoContext = createContext(null);
 export const UserInfoProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState({
     id: "testid",
+    group: "연암공과대학교",
     userName: "testuserName",
     account: "6127703304958",
   });
